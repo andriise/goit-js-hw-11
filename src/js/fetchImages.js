54693ed5205beadd7560bf9d9fa18a1fb2077c8d
Andrii Marchuk <andrii.marchuck@gmail.com>
2023-01-24 15:31:29 +0200

    Commit message

---
+++ src/js/fetchImages.js
@@ -0,0 +1,12 @@
+export const fetchImages = async (inputValue, pageNr) => {
+  return await fetch(
+    `https://pixabay.com/api/?key=29588079-fbc492831fdad231bf7222b96&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
+  )
+    .then(async response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return await response.json();
+    })
+ 
+};
